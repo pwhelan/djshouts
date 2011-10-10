a9826c5474db666a73e0b2945f40969012718d6d
Phillip Whelan <pwhelan@mixxx.org>
2011-10-10 03:01:46 -0700

    Adding the new URL for showing a 'show' with Open Graph support.

--- deejaypages/views.py
+++ deejaypages/views.py
@@ -11,8 +11,13 @@ def list_shows(request):
 		{'shows': shows, 'form': CreateShowForm()}
 	)
 
-def view_show(request):
-	show = Show.object.get(id__exact=1)
+def view_show(request, id):
+	show = Show.objects.get(id__exact=id)
+	hosturl = ('https' if request.is_secure() else 'http') + '://' + request.get_host()
+	flashvars = "lang=en&codec=mp3&volume=100&traking=false&jsevents=true&" + \
+			"buffering=5&title=Renegade%20Radio UK&welcome=Welcome%20To the Radio"
+	return direct_to_template(request, 'deejaypages/show.html', 
+				{'show': show, 'flashvars' : flashvars, 'hosturl' : hosturl})
 
 def create_show(request):
 	if request.method == 'POST':

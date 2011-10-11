1f42c3786cd4d0f90bc89c58f970a20d3d68e147
Phillip Whelan <pwhelan@mixxx.org>
2011-10-11 02:26:37 -0700

    Make all times use the America/Vancouver TZ.

--- deejaypages/models.py
+++ deejaypages/models.py
@@ -1,4 +1,7 @@
 import datetime
+from pytz import timezone
+import pytz
+
 from django.db import models
 from django.contrib.auth.models import User
 from djangotoolbox.fields import BlobField
@@ -12,7 +15,17 @@ class Show(models.Model):
 	dj = models.ForeignKey(DJ, null=True, blank=True)
 	url = models.CharField(max_length=256)
 	description = models.TextField()
-	date = models.DateTimeField()
+	
+	tz = timezone('America/Vancouver')
+	d = datetime.datetime.now(tz)
+	if d.minute < 15:
+		d = datetime.datetime(d.year, d.month, d.day, d.hour, 0)
+	elif d.minute >= 15 and d.minute < 45:
+		d = datetime.datetime(d.year, d.month, d.day, d.hour, 30)
+	else:
+		d = datetime.datetime(d.year, d.month, d.day, d.hour+1, 0)
+	
+	date = models.DateTimeField(default=d)
 	duration = models.TimeField()
 	
 	def end(self):

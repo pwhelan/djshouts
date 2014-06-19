b9365159f09a46d435d935f72926cd3befddbba7
Phillip Whelan <pwhelan@mixxx.org>
2014-06-18 23:57:55 -0700

    Remove unused code.

--- deejaypages/views.py
+++ deejaypages/views.py
@@ -1,13 +1,10 @@
-from django.views.generic.simple import direct_to_template
-from django.http import HttpResponseRedirect, HttpResponse
-from django.views.generic.simple import direct_to_template
-from deejaypages.forms import EditDJForm
-from deejaypages.models import DJ, OAuth2Access, TOKEN_AUTHORIZE, TOKEN_ACCESS, TOKEN_REFRESH
-from  django.core.exceptions import ObjectDoesNotExist
+# Really should do more with this...
+# 
+
+from django.http import HttpResponseRedirect
 
 def index(request):
 	if not request.user.is_authenticated():
-		return HttpResponseRedirect('/dj/me') # return direct_to_template(request, 'deejaypages/logind.html', {})
+		return HttpResponseRedirect('/dj/me')
 	else:
 		return HttpResponseRedirect('/dj/me')
-

b54d9c77191c2df3b1d79a081bed75c310af9e45
Phillip Whelan <pwhelan@mixxx.org>
2014-09-07 01:06:39 -0700

    Add properties to OAuth2 Connection model.

--- models/OAuth2/Connection.php
+++ models/OAuth2/Connection.php
@@ -2,6 +2,24 @@
 
 namespace Deejaypages\OAuth2;
 
-class Connection extends \Datachore\Model
+use Datachore\Type;
+use Datachore\Model;
+
+
+class Connection extends Model
 {
+	protected $properties = [
+		/** User reference **/
+		'user'		=> Type::Key,
+		/** Service Reference **/
+		'service'	=> Type::Key,
+		/** External ID **/
+		'xid'		=> Type::String,
+		/** Name **/
+		'name'		=> Type::String,
+		/** Type (as a string) **/
+		'type'		=> Type::String,
+		/** Is Hidden ? **/
+		'is_hidden'	=> Type::Boolean
+	];
 }
